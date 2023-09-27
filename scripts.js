db9074108f6e1a04ba08434c88f6fb77db45ad2e
Allen Stevens <allen.stevens@students.dominican.edu>
2023-09-27 14:38:37 -0700

    updated index file

--- scripts.js
+++ scripts.js
@@ -5,26 +5,36 @@ const itemsContainer = document.querySelector('#items')
 for (let i = 0; i < data.length; i += 1) {
 	// create a new div element and give it a class name
 	const newDiv = document.createElement('div');
-	newDiv.className = 'item'
+	newDiv.className = 'item';
 	// create an image element
 	const img = document.createElement('img');
 	// this will change each time we go through the loop. Can you explain why?
-	img.src = data[i].image
-	img.width = 300
-	img.height = 300
+	img.src = data[i].image;
+	img.width = 300;
+	img.height = 300;
 
-    const desc = document.createElement('P')
+const desc = document.createElement('P');
 // give the paragraph text from the data
-desc.innerText = data[i].desc
+desc.innerText = data[i].desc;
 // append the paragraph to the div
-newDiv.appendChild(desc)
+newDiv.appendChild(desc);
 // do the same thing for price
-const price = document.createElement('P')
-price.innerText = data[i].price
-newDiv.appendChild(price)
+const price = document.createElement('P');
+price.innerText = data[i].price;
+newDiv.appendChild(price);
+
+const button = document.createElement('button');
+button.id = data[i].name;
+button.setAttribute('data-price', data[i].price);
+button.innerText = 'Add to Cart';
+button.addEventListener('click', function(){
+    const moodPricee = parseFloat(this.getAttribute('data-pricee'));
+});
 
 	// Add the image to the div
-	newDiv.appendChild(img)
-	console.log(img) // Check the console!
-	itemsContainer.appendChild(newDiv)
+    newDiv.appendChild(img);
+	console.log(img); // Check the console!
+	itemsContainer.appendChild(newDiv);
+    
+    
 }
