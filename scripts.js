045b3cac8216410b5b7602dd11b7c77bb314ab6d
Allen Stevens <allen.stevens@students.dominican.edu>
2023-09-27 14:45:09 -0700

    displayed moods with button using javascript

--- scripts.js
+++ scripts.js
@@ -12,29 +12,25 @@ for (let i = 0; i < data.length; i += 1) {
 	img.src = data[i].image;
 	img.width = 300;
 	img.height = 300;
+    newDiv.appendChild(img)
+    itemsContainer.appendChild(newDiv)
 
-const desc = document.createElement('P');
-// give the paragraph text from the data
-desc.innerText = data[i].desc;
-// append the paragraph to the div
-newDiv.appendChild(desc);
-// do the same thing for price
-const price = document.createElement('P');
-price.innerText = data[i].price;
-newDiv.appendChild(price);
-
-const button = document.createElement('button');
-button.id = data[i].name;
-button.setAttribute('data-price', data[i].price);
-button.innerText = 'Add to Cart';
-button.addEventListener('click', function(){
-    const moodPricee = parseFloat(this.getAttribute('data-pricee'));
-});
+    const desc = document.createElement('P');
+    // give the paragraph text from the data
+    desc.innerText = data[i].desc;
+    // append the paragraph to the div
+    newDiv.appendChild(desc);
+    // do the same thing for price
+    const price = document.createElement('P');
+    price.innerText = data[i].price;
+    newDiv.appendChild(price);
+    // make a button
+    const button = document.createElement('button');
+    button.id = data[i].name;
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+}
 
 	// Add the image to the div
-    newDiv.appendChild(img);
-	console.log(img); // Check the console!
-	itemsContainer.appendChild(newDiv);
-    
-    
-}
+   
